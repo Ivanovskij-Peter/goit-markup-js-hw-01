5fccf4a665baac4225d2d92cd6d8b6a58ffc5cb4
Peter Ivanovskij <65976810+Ivanovskij-Peter@users.noreply.github.com>
2020-07-16 17:52:04 +0300

    начали разбирать js

---
+++ module01/index.js
@@ -0,0 +1,64 @@
+// let a = 24;
+// let b = "1";
+// console.log(a % 3);
+// console.log(typeof (a / b));
+
+// let a = 12;
+// let b = 3;
+// let result = a + b;
+// console.log(result);
+
+// let p = 3.14;
+// let r = 10;
+// let S = Math.pow(r, 2) * p;
+// console.log(S);
+
+// let b = `А`;
+// let c = `Б`;
+// let d = `Ц`;
+// console.log(
+//   `здраствуйте меня зовут ${b},мне ${c}, лет и мне нравиться ${d}, где ${b}, ${c}, ${d} - переменные вставленные в строку `
+// );
+// let a = !1;
+// let b = 0;
+// console.log(a === b);
+
+// let a;
+// let b = true;
+// let c = a || b;
+// console.log(c);
+
+// let a;
+// let b = true;
+// let c = a && b;
+// console.log(c);
+
+// let cost = 0;
+// const subscription = "pro";
+// if (subscription === "pro") {
+//   cost = 100;
+// }
+// console.log(cost);
+
+// let cost = 0;
+// const number = 5;
+// number > 10 ? (cost = 100) : console.log("false");
+// console.log(cost);
+
+// let a = 1;
+
+// if (true) {
+//   a = 2;
+// }
+// console.log(a);
+
+// let x = 10;
+// while (x != 0) {
+//   x--;
+//   console.log(x);
+// }
+
+// const max = 10;
+// for (let i = 0; i < 5; ++i) {
+//   console.log(i);
+// }
