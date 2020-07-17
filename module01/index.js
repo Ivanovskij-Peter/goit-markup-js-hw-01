a269c25d29ad305215292863da0f882263284c61
Peter Ivanovskij <65976810+Ivanovskij-Peter@users.noreply.github.com>
2020-07-17 15:56:11 +0300

    пробовал решать задачи

--- module01/index.js
+++ module01/index.js
@@ -1,1 +1,56 @@
+// const ask = prompt("Какое официальное значение Javascript?");
+// let name = ECMAscript;
+// if (ask === ECMAscript) {
+//   console.log("Верно!");
+// } else
+// {
+//   console.log(`Не знаете? ${name}`);
+// }
 
+// let a = 1;
+// let b = 2;
+// const result = a + b < 4 ? "много" : "мало";
+// console.log(result);
+
+// let result = "ok";
+// let age = 30;
+// if (age >= 14 && age <= 90) {
+//   alert(result);
+// }
+// -------11
+// const password = "Я главный";
+// let ask = prompt("Введте логин");
+// if (ask === null) {
+//   console.log(`Отменено`);
+// }
+// if (ask === "") {
+//   console.log(`Я вас не знаю`);
+// } else {
+//   if (ask === password) {
+//     console.log(`здраствуйте`);
+//   }
+//   if (ask !== password) {
+//     console.log(`Неверный пароль`);
+//   }
+// }
+// --------13
+// const max = 10;
+// for (let i = 2; i <= max; i += 2) {
+//   console.log(i);
+// }
+
+// -----------14
+// const num = 100;
+// while (true) {
+//   let ask = prompt("Введите число больше 100");
+//   if (ask === null) {
+//     break;
+//   }
+//   if (ask >= num) {
+//     break;
+//   }
+// }
+// let i = 3;
+// while (i) {
+//   alert(i--);
+// }
