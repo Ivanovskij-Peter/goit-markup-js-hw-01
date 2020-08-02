e8bba663232465deb119e316a4d37d7f3f4d35d8
Peter Ivanovskij <65976810+Ivanovskij-Peter@users.noreply.github.com>
2020-08-02 22:24:47 +0300

    Update task05.js

--- module05/task05.js
+++ module05/task05.js
@@ -198,7 +198,7 @@
 //   }
 //   drive(hours) {
 //     if (this.isOn === true) {
-//       this.distance = hours * this.speed;
+//       this.distance += hours * this.speed;
 //     }
 //   }
 // }
@@ -217,7 +217,7 @@
 // mustang.turnOff();
 
 // Car.getSpecs(mustang);
-// maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000
+// // maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000
 
 // console.log(mustang.price); // 2000
 // mustang.price = 4000;
